c4e9b5c021eb829eb66a6a4f9a36a96157a184d3
Michelle Nguyen <michellenguyen11239@aol.com>
2021-05-18 17:04:10 -0700

    Added chalk package and added validation for prompts

--- src/questions.js
+++ src/questions.js
@@ -1,25 +1,36 @@
 const inquirer = require('inquirer'); // Require package 
+const chalk = require('chalk');
 
+const validateInput = (input) => { // Validation for if the user doesn't enter a value
+    if (!input) {
+        return chalk.red("Please enter a value.");
+      }
+      return true;
+}
 let mQuestions = [
     {
         name: 'name', 
         message: 'What is the name of the team manager?',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
     },
     {
         name: 'id',
         message: 'What is the ID of the team manager?',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
     },
     { 
         name: 'email',
         message: "What is the team manager's email?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
     },
     { 
         name: 'office',
         message: "What is the team manager's office number?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
     },
     { 
         name: 'choice',
@@ -33,22 +44,26 @@ let eQuestions = [
     {
         name: 'name', 
         message: 'What is the name of the engineer?',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         {
         name: 'id',
         message: 'What is the ID of the engineer?',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'email',
         message: "What is the engineer's email?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'github',
         message: "What is the engineer's github?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'choice',
@@ -61,22 +76,26 @@ let iQuestions = [
     {
         name: 'name', 
         message: 'What is the name of the intern?',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         {
         name: 'id',
         message: 'What is the ID of the intern',
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'email',
         message: "What is the intern's email?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'school',
         message: "What school does the intern go to?",
-        type: 'input'
+        type: 'input',
+        validate: validateInput
         },
         { 
         name: 'choice',
